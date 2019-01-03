df07431e492f8de0e83a314b38c0a309e3265bde
120173721@qq.com <120173721@qq.com>
2019-01-03 20:00:33 +0800

    BB平台升级

--- TestDemo/Form1.Designer.cs
+++ TestDemo/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(72, 164);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 40);
             this.button1.TabIndex = 0;
             this.button1.Text = "同意";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,21 +46,24 @@
             // 
             this.button2.Location = new System.Drawing.Point(178, 164);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 40);
             this.button2.TabIndex = 1;
             this.button2.Text = "算了吧";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button2_MouseMove);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 224);
+            this.ClientSize = new System.Drawing.Size(432, 274);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
---
+++ TestDemo/FrmFullScreen.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace TestDemo
+{
+    partial class FrmFullScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Image = global::TestDemo.Properties.Resources._01252f5752f1106ac72525aec1a444;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 261);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Timo 装逼专用啊\r\n按F11退出全屏\r\n按ESC退出程序\r\n";
+            // 
+            // FrmFullScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmFullScreen";
+            this.Text = "FrmFullScreen";
+            this.Load += new System.EventHandler(this.btnFullScreen_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnFullScreen_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
